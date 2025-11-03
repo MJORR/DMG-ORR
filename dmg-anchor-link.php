bf1db5d7c3f7147c6a1d78c7043fb867ba022689
MJORR <magnusjackorr@gmail.com>
2025-11-03 07:07:20 +0000

    Add plugin description

--- dmg-anchor-link.php
+++ dmg-anchor-link.php
@@ -2,9 +2,10 @@
 
 /**
  * Plugin Name:       DMG Anchor Link
- * Description:       Add stylized anchor links to your WordPress content.
+ * Description:       A high-performance WordPress plugin that provides a Gutenberg block for adding stylized anchor links to posts, plus a WP-CLI command for searching posts containing these blocks at scale.
  * Version:           0.1.0
  * Author:            Magnus J. Orr
+ * Author URI:        https://magnusorr.com
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       dmg-anchor-link
